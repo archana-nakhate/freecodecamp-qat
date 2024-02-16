c32a49c3f4122af65bec04905429fb3f5ea978ff
archana-nakhate <archanaa.pawar@gmail.com>
2024-02-16 12:47:42 +1100

    tests/2_functional-tests.js #3

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -50,13 +50,21 @@ suite('Functional Tests', function () {
     });
     // #4
     test('Send {surname: "da Verrazzano"}', function (done) {
-      assert.fail();
-
-      done();
+      chai
+         .request(server)
+         .keepOpen()
+         .put('/travellers')
+         .send({surname: "da Verrazzano"})
+         .end(function(err, res){
+              assert.equal(res.status, 200);
+              assert.equal(res.type, "application/json");
+              assert.equal(res.body.name, "Giovanni");
+              assert.equal(res.body.surname, "da Verrazzano");
+          done();
+        });
     });
   });
 });
-
 const Browser = require('zombie');
 
 suite('Functional Tests with Zombie.js', function () {
