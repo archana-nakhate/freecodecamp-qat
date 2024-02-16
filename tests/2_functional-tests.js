7a2c7abd7339823d9ee12a13d29f6c3ff7b4b972
archana-nakhate <archanaa.pawar@gmail.com>
2024-02-16 13:43:15 +1100

    tests/2_functional-tests.js #5

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -84,7 +84,6 @@ suite('Functional Tests with Zombie.js', function () {
     // #5
     test('Submit the surname "Colombo" in the HTML form', function (done) {
           browser.fill('surname', 'Colombo');
-
           browser.pressButton('submit', () => {
             browser.assert.success();
             browser.assert.text('span#name', 'Cristoforo');
@@ -96,9 +95,15 @@ suite('Functional Tests with Zombie.js', function () {
 
     // #6
     test('Submit the surname "Vespucci" in the HTML form', function (done) {
-      assert.fail();
+     browser.fill('surname', 'Vespucci');
+               browser.pressButton('submit', () => {
+                 browser.assert.success();
+                 browser.assert.text('span#name', 'Amerigo');
+                 browser.assert.text('span#surname', 'Vespucci');
+                 browser.assert.elements('span#dates', 1);
 
       done();
     });
   });
+});
 });
